4a495a6f98ffe58b2ffd76c3586ef850d0030823
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-09 19:52:26 +0330

    create new products area

---
+++ src/components/NewProducts/NewProducts.js
@@ -0,0 +1,321 @@
+import React from "react";
+import "./NewProducts.css";
+function TopProducts() {
+  return (
+    <section class="top-products-area pt-100 pb-70">
+      <div class="container">
+        <div class="section-title">
+          <h2>New Products</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-4 col-sm-6">
+            <div class="single-top-products">
+              <div class="top-products-image">
+                <a href="products-details.html">
+                  <img
+                    src="https://theme.partlinks.com.au/assets/img/top-products/top-products-1.jpg"
+                    alt="image"
+                  />
+                </a>
+
+                <ul class="top-products-action">
+                  <li>
+                    <a href="cart.html">
+                      <i class="flaticon-shopping-cart"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="wishlist.html">
+                      <i class="flaticon-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="top-products-content">
+                <h3>
+                  <a href="products-details.html">17 Inch Rim</a>
+                </h3>
+                <ul class="rating">
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bx-star"></i>
+                  </li>
+                </ul>
+                <span>$89.00</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <div class="single-top-products">
+              <div class="top-products-image">
+                <a href="products-details.html">
+                  <img
+                    src="https://theme.partlinks.com.au/assets/img/top-products/top-products-2.jpg"
+                    alt="image"
+                  />
+                </a>
+
+                <ul class="top-products-action">
+                  <li>
+                    <a href="cart.html">
+                      <i class="flaticon-shopping-cart"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="wishlist.html">
+                      <i class="flaticon-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="top-products-content">
+                <h3>
+                  <a href="products-details.html">Lubricants</a>
+                </h3>
+                <ul class="rating">
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bx-star"></i>
+                  </li>
+                </ul>
+                <span>$99.00</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <div class="single-top-products">
+              <div class="top-products-image">
+                <a href="products-details.html">
+                  <img
+                    src="https://theme.partlinks.com.au/assets/img/top-products/top-products-3.jpg"
+                    alt="image"
+                  />
+                </a>
+
+                <ul class="top-products-action">
+                  <li>
+                    <a href="cart.html">
+                      <i class="flaticon-shopping-cart"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="wishlist.html">
+                      <i class="flaticon-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="top-products-content">
+                <h3>
+                  <a href="products-details.html">Isphat Rim</a>
+                </h3>
+                <ul class="rating">
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bx-star"></i>
+                  </li>
+                </ul>
+                <span>$55.00</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <div class="single-top-products">
+              <div class="top-products-image">
+                <a href="products-details.html">
+                  <img
+                    src="https://theme.partlinks.com.au/assets/img/top-products/top-products-7.jpg"
+                    alt="image"
+                  />
+                </a>
+
+                <ul class="top-products-action">
+                  <li>
+                    <a href="cart.html">
+                      <i class="flaticon-shopping-cart"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="wishlist.html">
+                      <i class="flaticon-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="top-products-content">
+                <h3>
+                  <a href="products-details.html">Car Engine</a>
+                </h3>
+                <ul class="rating">
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bx-star"></i>
+                  </li>
+                </ul>
+                <span>$38.00</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <div class="single-top-products">
+              <div class="top-products-image">
+                <a href="products-details.html">
+                  <img
+                    src="https://theme.partlinks.com.au/assets/img/top-products/top-products-8.jpg"
+                    alt="image"
+                  />
+                </a>
+
+                <ul class="top-products-action">
+                  <li>
+                    <a href="cart.html">
+                      <i class="flaticon-shopping-cart"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="wishlist.html">
+                      <i class="flaticon-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="top-products-content">
+                <h3>
+                  <a href="products-details.html">Wheel</a>
+                </h3>
+                <ul class="rating">
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bx-star"></i>
+                  </li>
+                </ul>
+                <span>$35.00</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-sm-6">
+            <div class="single-top-products">
+              <div class="top-products-image">
+                <a href="products-details.html">
+                  <img
+                    src="https://theme.partlinks.com.au/assets/img/top-products/top-products-10.jpg"
+                    alt="image"
+                  />
+                </a>
+
+                <ul class="top-products-action">
+                  <li>
+                    <a href="cart.html">
+                      <i class="flaticon-shopping-cart"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="wishlist.html">
+                      <i class="flaticon-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="top-products-content">
+                <h3>
+                  <a href="products-details.html">Metal-3d-Car Render</a>
+                </h3>
+                <ul class="rating">
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bxs-star"></i>
+                  </li>
+                  <li>
+                    <i class="bx bx-star"></i>
+                  </li>
+                </ul>
+                <span>$55.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TopProducts;

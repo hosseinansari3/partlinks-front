f912088833e378ed169285408ae3cc65ffe3d606
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-09 22:57:07 +0330

    edite imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ import Navbar from "../components/Navbar/Navbar";
 import MainBanner from "../components/MainBanner/MainBanner";
 import Support from "../components/Support/Support";
 import WorkArea from "../components/WorkArea/WorkArea";
-import TopProducts from "../components/NewProducts/NewProducts";
+import NewProducts from "../components/NewProducts/NewProducts";
+import Overview from "../components/Overview/Overview";
+import PartnerArea from "../components/PartnerArea/PartnerArea";
 
 function Home() {
   return (
@@ -14,7 +16,9 @@ function Home() {
       <MainBanner />
       <Support />
       <WorkArea />
-      <TopProducts />
+      <NewProducts />
+      <Overview />
+      <PartnerArea />
       Home
     </div>
   );

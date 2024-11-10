50a5e8a10e9d9208a409e0b9dad5f7910a0edd79
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-10 23:33:12 +0330

    create about us page

---
+++ src/pages/About/About.js
@@ -0,0 +1,148 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div>
+      <div class="page-banner-area">
+        <div class="d-table">
+          <div class="d-table-cell">
+            <div class="container">
+              <div class="page-banner-content">
+                <h2>About Us</h2>
+                <ul>
+                  <li>
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li>About Us</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="story-area ptb-100">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-6">
+              <div class="story-image">
+                <a
+                  href="https://www.youtube.com/watch?v=BVMsRltq2yU"
+                  class="video-btn popup-youtube"
+                >
+                  <i class="bx bx-play-circle"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+              <div class="story-content">
+                <h3>Our Story</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+                  maecenas accumsan lacus vel facilisis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="mission-area pb-100">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-6">
+              <div class="mission-content">
+                <h3>Our Mission</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+                  maecenas accumsan lacus vel facilisis.
+                </p>
+
+                <ul class="mission-list">
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </li>
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </li>
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Quis ipsum suspendisse ultrices gravida.
+                  </li>
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+              <div class="mission-image"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="vision-area pb-100">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-6">
+              <div class="vision-image"></div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+              <div class="vision-content">
+                <h3>Our Vision</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+                  maecenas accumsan lacus vel facilisis.
+                </p>
+
+                <ul class="vision-list">
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </li>
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </li>
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Quis ipsum suspendisse ultrices gravida.
+                  </li>
+                  <li>
+                    <i class="bx bx-check"></i>
+                    Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default About;

bf74f3182f1e75f395deb0fe5a0b84fbffcb4e18
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-10 13:55:02 +0330

    create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,146 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <section class="footer-area pt-100 pb-70">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-3 col-sm-6">
+            <div class="single-footer-widget">
+              <a href="index.html">
+                <img
+                  src="https://templates.hibootstrap.com/maxon/default/assets/img/logo-2.png"
+                  alt="image"
+                />
+              </a>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+
+              <ul class="footer-social">
+                <li>
+                  <a href="https://www.facebook.com/login/" target="_blank">
+                    <i class="bx bxl-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/i/flow/login" target="_blank">
+                    <i class="bx bxl-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.pinterest.com/" target="_blank">
+                    <i class="bx bxl-pinterest-alt"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/" target="_blank">
+                    <i class="bx bxl-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-sm-6">
+            <div class="single-footer-widget">
+              <h2>Recent Product</h2>
+
+              <div class="footer-post">
+                <article class="item">
+                  <a href="blog-details.html" class="thumb">
+                    <span class="fullimage cover bg1" role="img"></span>
+                  </a>
+                  <div class="info">
+                    <h4 class="title usmall">
+                      <a href="blog-details.html">
+                        Electronic Car Protect Air Pollution
+                      </a>
+                    </h4>
+                    <span>24 Dec 2024</span>
+                  </div>
+                </article>
+
+                <article class="item">
+                  <a href="blog-details.html" class="thumb">
+                    <span class="fullimage cover bg2" role="img"></span>
+                  </a>
+                  <div class="info">
+                    <h4 class="title usmall">
+                      <a href="blog-details.html">
+                        Automotive Advancements to Look Forward
+                      </a>
+                    </h4>
+                    <span>25 Dec 2024</span>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-sm-6">
+            <div class="single-footer-widget pl-5">
+              <h2>Quick Links</h2>
+
+              <ul class="quick-links">
+                <li>
+                  <i class="bx bxs-chevrons-right"></i>
+                  <a href="about.html">About Company</a>
+                </li>
+                <li>
+                  <i class="bx bxs-chevrons-right"></i>
+                  <a href="blog-details.html">Services</a>
+                </li>
+                <li>
+                  <i class="bx bxs-chevrons-right"></i>
+                  <a href="shop.html">Shop</a>
+                </li>
+                <li>
+                  <i class="bx bxs-chevrons-right"></i>
+                  <a href="faq.html">FAQ</a>
+                </li>
+                <li>
+                  <i class="bx bxs-chevrons-right"></i>
+                  <a href="blog.html">Blog</a>
+                </li>
+                <li>
+                  <i class="bx bxs-chevrons-right"></i>
+                  <a href="gallery.html">Gallery</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-sm-6">
+            <div class="single-footer-widget">
+              <h2>Information</h2>
+
+              <ul class="footer-contact-info">
+                <li>
+                  <i class="bx bxs-phone"></i>
+                  <span>Phone</span>
+                  <a href="tel:882569756">882-569-756</a>
+                </li>
+                <li>
+                  <i class="bx bx-envelope"></i>
+                  <span>Email</span>
+                  <a href="mailto:hello@maxon.com">hello@maxon.com</a>
+                </li>
+                <li>
+                  <i class="bx bx-map"></i>
+                  <span>Address</span>
+                  175 5th Ave Premium Area, New York
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;

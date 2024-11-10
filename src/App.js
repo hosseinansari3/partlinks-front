8da5c3d68de5aa5661c216f3a6bd587e17789002
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-10 23:34:20 +0330

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import About from "./pages/About/About";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Contact from "./pages/Contact/Contact";
+import SellCar from "./pages/SellCar/SellCar";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route exact index path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="sell-car" element={<SellCar />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./responsive.css";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );

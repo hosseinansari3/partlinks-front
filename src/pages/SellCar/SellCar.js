5df3dd71bb34e66c2a7e6f2df5139765747b2dd7
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-12 00:39:30 +0330

    create second step in sellcar page

--- src/pages/SellCar/SellCar.js
+++ src/pages/SellCar/SellCar.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { alpha, styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import { useForm } from "react-hook-form";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const STEPS = {
+  STEP_CAR: 0,
+  STEP_CAR_1: 1,
+  STEP_USER: 2,
+};
 
 const BootstrapInput = styled(InputBase)(({ theme }) => ({
   "label + &": {
@@ -12,7 +19,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
   "& .MuiInputBase-input": {
     borderRadius: 4,
     position: "relative",
-    backgroundColor: "#F3F6F9",
+    backgroundColor: "white",
     border: "1px solid",
     borderColor: "#E0E3E7",
     fontSize: 16,
@@ -57,7 +64,245 @@ function SellCar() {
     formState: { errors },
   } = useForm();
 
+  const [step, setStep] = useState(STEPS.STEP_CAR);
+
   const state = watch("state", "");
+
+  const cardContent =
+    step == STEPS.STEP_CAR ? (
+      <div className="step-car">
+        <div className="row">
+          <div className="col-12">
+            <div className="row mb-2">
+              <div className="col-6">
+                <InputLabel htmlFor="plate-number">Plate Number</InputLabel>
+                <BootstrapInput
+                  fullWidth
+                  placeholder="Enter Plate Number"
+                  id="plate-number"
+                />
+              </div>
+              <div className="col-6">
+                <InputLabel id="demo-simple-select-label">State</InputLabel>
+                <Select
+                  {...register("state", { required: true })}
+                  fullWidth
+                  input={<BootstrapInput />}
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  //inputProps={{ "aria-label": "Without label" }}
+                  //defaultValue={0}
+                  displayEmpty
+                  value={state}
+                >
+                  <MenuItem hidden value="">
+                    Select State
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </div>
+            </div>
+            <div class="d-flex flex-column mb-2">
+              <label class="d-flex align-items-center fs-6 fw-semibold mb-2 mt-2">
+                <b>OR</b>
+              </label>
+            </div>
+            <div className="d-flex flex-column mb-5">
+              <InputLabel htmlFor="vin-number">VIN</InputLabel>
+              <BootstrapInput
+                fullWidth
+                placeholder="Enter vin number"
+                id="vin-number"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : step == STEPS.STEP_CAR_1 ? (
+      <div className="step-car2">
+        <div className="row mb-2">
+          <div className="col-6 mb-3">
+            <InputLabel htmlFor="make">
+              <span>Make</span>
+            </InputLabel>
+            <Autocomplete
+              sx={{
+                "label + &": {
+                  marginTop: "8px",
+                },
+                "& .MuiOutlinedInput-root": {
+                  padding: "3px 12px", // Set your desired padding here
+                },
+              }}
+              disablePortal
+              options={["aaa", "rrrr", "www", "oooo", "nnnn"]}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Select a make..."
+                  InputProps={{
+                    ...params.InputProps,
+                    sx: {
+                      "& .MuiAutocomplete-input": {
+                        padding: "10px 12px", // Set your desired padding here
+                      },
+                    },
+                  }}
+                />
+              )}
+            />
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel htmlFor="model">
+              <span>Model</span>
+            </InputLabel>
+            <Autocomplete
+              sx={{
+                "label + &": {
+                  marginTop: "8px",
+                },
+                "& .MuiOutlinedInput-root": {
+                  padding: "3px 12px", // Set your desired padding here
+                },
+              }}
+              id="model"
+              disablePortal
+              options={["aaa", "rrrr", "www", "oooo", "nnnn"]}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Select a model..."
+                  sx={{
+                    "& .MuiOutlinedInput-root .MuiAutocomplete-input": {
+                      padding: "10px 12px", // Set your desired padding here
+                    },
+                  }}
+                />
+              )}
+            />
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel htmlFor="year">
+              <span>Year</span>
+            </InputLabel>
+            <Autocomplete
+              sx={{
+                "label + &": {
+                  marginTop: "8px",
+                },
+                "& .MuiOutlinedInput-root": {
+                  padding: "3px 12px", // Set your desired padding here
+                },
+              }}
+              id="year"
+              disablePortal
+              options={["aaa", "rrrr", "www", "oooo", "nnnn"]}
+              renderInput={(params) => (
+                <TextField {...params} placeholder="Select Year" />
+              )}
+            />
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel id="body-type">Body Type</InputLabel>
+            <Select
+              {...register("state", { required: true })}
+              fullWidth
+              input={<BootstrapInput />}
+              labelId="body-type"
+              id="body-type"
+              //inputProps={{ "aria-label": "Without label" }}
+              //defaultValue={0}
+              displayEmpty
+              value={state}
+            >
+              <MenuItem hidden value="">
+                Select Body Type
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel id="transmission">Transmission</InputLabel>
+            <Select
+              {...register("state", { required: true })}
+              fullWidth
+              input={<BootstrapInput />}
+              labelId="transmission"
+              id="transmission"
+              //inputProps={{ "aria-label": "Without label" }}
+              //defaultValue={0}
+              displayEmpty
+              value={state}
+            >
+              <MenuItem hidden value="">
+                Select Transmission
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel id="fuel">Fuel</InputLabel>
+            <Select
+              {...register("state", { required: true })}
+              fullWidth
+              input={<BootstrapInput />}
+              labelId="fuel"
+              id="fuel"
+              //inputProps={{ "aria-label": "Without label" }}
+              //defaultValue={0}
+              displayEmpty
+              value={state}
+            >
+              <MenuItem hidden value="">
+                Select Fuel
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel htmlFor="cylinders">
+              <span>cylinders</span>
+            </InputLabel>
+            <BootstrapInput fullWidth id="cylinders" />
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel htmlFor="series">
+              <span>series</span>
+            </InputLabel>
+            <BootstrapInput fullWidth id="series" />
+          </div>
+          <div className="col-6 mb-3">
+            <InputLabel htmlFor="color">
+              <span>color</span>
+            </InputLabel>
+            <BootstrapInput fullWidth id="plate-number" />
+          </div>
+        </div>
+      </div>
+    ) : null;
+
+  const onBack = useCallback(() => {
+    setStep((value) => value - 1);
+  }, []);
+
+  const onNext = useCallback(() => {
+    setStep((value) => value + 1);
+  }, []);
+
+  const onSubmit = (e) => {
+    console.log("Onsubmit", step);
+    if (step !== STEPS.STEP_USER) {
+      return onNext();
+    }
+  };
 
   return (
     <div>
@@ -76,76 +321,23 @@ function SellCar() {
               </div>
               <div className="card-body">
                 <div className="mt-5">
-                  <div className="mt-2 mb-2">
-                    <div className="step-car">
-                      <div className="row">
-                        <div className="col-12">
-                          <div className="row mb-2">
-                            <div className="col-6">
-                              <InputLabel htmlFor="plate-number">
-                                Plate Number
-                              </InputLabel>
-                              <BootstrapInput
-                                fullWidth
-                                placeholder="Enter Plate Number"
-                                id="plate-number"
-                              />
-                            </div>
-                            <div className="col-6">
-                              <InputLabel id="demo-simple-select-label">
-                                State
-                              </InputLabel>
-
-                              <Select
-                                {...register("state", { required: true })}
-                                fullWidth
-                                input={<BootstrapInput />}
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                //inputProps={{ "aria-label": "Without label" }}
-                                //defaultValue={0}
-                                displayEmpty
-                                value={state}
-                              >
-                                <MenuItem hidden value="">
-                                  Select State
-                                </MenuItem>
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
-                              </Select>
-                            </div>
-                          </div>
-                          <div class="d-flex flex-column mb-2">
-                            <label class="d-flex align-items-center fs-6 fw-semibold mb-2 mt-2">
-                              <b>OR</b>
-                            </label>
-                          </div>
-                          <div className="d-flex flex-column mb-5">
-                            <InputLabel htmlFor="vin-number">VIN</InputLabel>
-                            <BootstrapInput
-                              fullWidth
-                              placeholder="Enter vin number"
-                              id="vin-number"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div
-                      class="d-flex justify-content-between align-items-center pt-2"
-                      id="info-content"
-                    >
-                      <div></div>
-                      <div>
-                        <button
-                          type="button"
-                          class="btn btn-sm btn-primary"
-                          id="continue-selling-car-btn"
-                        >
-                          <span class="indicator-label">
-                            Continue
-                            <span class="svg-icon svg-icon-4 ms-2">
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mt-2 mb-2">
+                      {cardContent}
+                      <div
+                        class="d-flex justify-content-between align-items-center pt-2"
+                        id="info-content"
+                      >
+                        <div className="mr-2">
+                          <button
+                            type="button"
+                            onClick={onBack}
+                            className={`${
+                              step == STEPS.STEP_CAR && "d-none"
+                            } btn btn-sm btn-warning me-3`}
+                            onclick="backToShowSellingCarForm()"
+                          >
+                            <span class="svg-icon svg-icon-4 me-1">
                               <svg
                                 width="24"
                                 height="24"
@@ -155,29 +347,64 @@ function SellCar() {
                               >
                                 <rect
                                   opacity="0.5"
-                                  x="18"
-                                  y="13"
+                                  x="6"
+                                  y="11"
                                   width="13"
                                   height="2"
                                   rx="1"
-                                  transform="rotate(-180 18 13)"
                                   fill="currentColor"
                                 ></rect>
                                 <path
-                                  d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z"
+                                  d="M8.56569 11.4343L12.75 7.25C13.1642 6.83579 13.1642 6.16421 12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75L5.70711 11.2929C5.31658 11.6834 5.31658 12.3166 5.70711 12.7071L11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25C13.1642 17.8358 13.1642 17.1642 12.75 16.75L8.56569 12.5657C8.25327 12.2533 8.25327 11.7467 8.56569 11.4343Z"
                                   fill="currentColor"
                                 ></path>
                               </svg>
                             </span>
-                          </span>
-                          <span class="indicator-progress d-none">
-                            Please wait...
-                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
-                          </span>
-                        </button>
+                            Back
+                          </button>
+                        </div>
+                        <div>
+                          <button
+                            type="submit"
+                            class="btn btn-sm btn-primary"
+                            id="continue-selling-car-btn"
+                          >
+                            <span class="indicator-label">
+                              Continue
+                              <span class="svg-icon svg-icon-4 ms-2">
+                                <svg
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <rect
+                                    opacity="0.5"
+                                    x="18"
+                                    y="13"
+                                    width="13"
+                                    height="2"
+                                    rx="1"
+                                    transform="rotate(-180 18 13)"
+                                    fill="currentColor"
+                                  ></rect>
+                                  <path
+                                    d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z"
+                                    fill="currentColor"
+                                  ></path>
+                                </svg>
+                              </span>
+                            </span>
+                            <span class="indicator-progress d-none">
+                              Please wait...
+                              <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                            </span>
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </form>
                 </div>
               </div>
             </div>

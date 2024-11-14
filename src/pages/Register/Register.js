2fd84b6ac83e3240722d0bacf1ff11410d74011f
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-14 13:18:42 +0330

    replace class with className

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -4,15 +4,15 @@ import "./Register.css";
 function Register() {
   return (
     <div className="d-flex flex-column flex-lg-row flex-column-fluid stepper stepper-pills stepper-column stepper-multistep">
-      <div class="d-flex flex-column flex-lg-row-auto w-lg-250px w-xl-350px ">
+      <div className="d-flex flex-column flex-lg-row-auto w-lg-250px w-xl-350px ">
         <div
-          class="d-flex flex-column position-lg-fixed top-0 bottom-0 w-lg-250px w-xl-350px scroll-y bgi-size-cover bgi-position-center"
+          className="d-flex flex-column position-lg-fixed top-0 bottom-0 w-lg-250px w-xl-350px scroll-y bgi-size-cover bgi-position-center"
           style={{
             backgroundImage:
               "url('https://partlinks.com.au/panel/media/misc/auth-bg.png')",
           }}
         >
-          <div class="d-flex justify-content-center align-items-center py-4 py-lg-5 mt-lg-5">
+          <div className="d-flex justify-content-center align-items-center py-4 py-lg-5 mt-lg-5">
             <a href="https://partlinks.com.au">
               <img
                 alt="Logo"
@@ -21,11 +21,11 @@ function Register() {
             </a>
           </div>
 
-          <div class="d-flex justify-content-center align-items-center flex-wrap fs-5 px-4 py-5">
-            <div class="d-flex fw-normal">
+          <div className="d-flex justify-content-center align-items-center flex-wrap fs-5 px-4 py-5">
+            <div className="d-flex fw-normal">
               <a
                 href="https://partlinks.com.au"
-                class="text-light px-5"
+                className="text-light px-5"
                 target="_blank"
               >
                 Register an account
@@ -35,18 +35,18 @@ function Register() {
         </div>
       </div>
       <div
-        class="d-flex flex-column flex-lg-row-fluid py-5"
+        className="d-flex flex-column flex-lg-row-fluid py-5"
         id="kt_create_account_form_stepper"
       >
-        <div class="d-flex justify-content-center align-items-center flex-column flex-column-fluid">
-          <div class="w-lg-700px w-xl-850px w-md-700px p-3 w-100 mx-auto">
-            <div class="current">
-              <div class="w-100">
-                <div class="pb-14 pb-lg-5">
-                  <h2 class=" d-flex align-items-center text-dark">
+        <div className="d-flex justify-content-center align-items-center flex-column flex-column-fluid">
+          <div className="w-lg-700px w-xl-850px w-md-700px p-3 w-100 mx-auto">
+            <div className="current">
+              <div className="w-100">
+                <div className="pb-14 pb-lg-5">
+                  <h2 className=" d-flex align-items-center text-dark">
                     Choose Account Type
                     <i
-                      class="fas fa-exclamation-circle ms-2 fs-7"
+                      className="fas fa-exclamation-circle ms-2 fs-7"
                       data-bs-toggle="tooltip"
                       aria-label="Choose your Account type correctly!"
                       data-bs-original-title="Choose your Account type correctly!"
@@ -54,30 +54,30 @@ function Register() {
                     ></i>
                   </h2>
 
-                  <div class="form-text text-danger">
-                    <span class="fa fa-info-circle mr-1"></span> You will not be
-                    able to change this after you complete registration
+                  <div className="form-text text-danger">
+                    <span className="fa fa-info-circle mr-1"></span> You will
+                    not be able to change this after you complete registration
                   </div>
                 </div>
 
-                <div class="fv-row">
-                  <div class="row">
-                    <div class="col-12 col-sm-6 col-md-2"></div>
-                    <div class="col-12 col-sm-6 col-md-4">
+                <div className="fv-row">
+                  <div className="row">
+                    <div className="col-12 col-sm-6 col-md-2"></div>
+                    <div className="col-12 col-sm-6 col-md-4">
                       <input
                         type="radio"
-                        class="btn-check"
+                        className="btn-check"
                         name="type"
                         value="member/business"
                         id="kt_create_account_form_type_member_business"
                         onchange="register_show_form(this.value)"
                       />
                       <label
-                        class="btn btn-active-light-warning p-4 d-flex align-items-center mb-4"
+                        className="btn btn-active-light-warning p-4 d-flex align-items-center mb-4"
                         for="kt_create_account_form_type_member_business"
                         style={{ border: "2px dashed" }}
                       >
-                        <span class="svg-icon svg-icon-3x me-3">
+                        <span className="svg-icon svg-icon-3x me-3">
                           <svg
                             width="24"
                             height="24"
@@ -97,29 +97,31 @@ function Register() {
                           </svg>
                         </span>
 
-                        <span class="d-block fw-semibold text-start">
-                          <span class="member-text text-dark d-block mb-1">
+                        <span className="d-block fw-semibold text-start">
+                          <span className="member-text text-dark d-block mb-1">
                             Business Member
                           </span>
-                          <span class="account-text">Business account.</span>
+                          <span className="account-text">
+                            Business account.
+                          </span>
                         </span>
                       </label>
                     </div>
-                    <div class="col-12 col-sm-6 col-md-4">
+                    <div className="col-12 col-sm-6 col-md-4">
                       <input
                         type="radio"
-                        class="btn-check"
+                        className="btn-check"
                         name="type"
                         value="member/private"
                         id="kt_create_account_form_type_member_private"
                         onchange="register_show_form(this.value)"
                       />
                       <label
-                        class="btn btn-active-light-warning p-4 d-flex align-items-center mb-4"
+                        className="btn btn-active-light-warning p-4 d-flex align-items-center mb-4"
                         for="kt_create_account_form_type_member_private"
                         style={{ border: "2px dashed" }}
                       >
-                        <span class="svg-icon svg-icon-3x me-3">
+                        <span className="svg-icon svg-icon-3x me-3">
                           <svg
                             width="24"
                             height="24"
@@ -139,33 +141,33 @@ function Register() {
                           </svg>
                         </span>
 
-                        <span class="d-block fw-semibold text-start">
-                          <span class="member-text text-dark d-block mb-1">
+                        <span className="d-block fw-semibold text-start">
+                          <span className="member-text text-dark d-block mb-1">
                             Private Member
                           </span>
-                          <span class="account-text">Private account.</span>
+                          <span className="account-text">Private account.</span>
                         </span>
                       </label>
                     </div>
-                    <div class="col-12 col-sm-6 col-md-2"></div>
+                    <div className="col-12 col-sm-6 col-md-2"></div>
                   </div>
-                  <div class="row">
-                    <div class="col-12 col-sm-6 col-md-2"></div>
-                    <div class="col-12 col-sm-6 col-md-4">
+                  <div className="row">
+                    <div className="col-12 col-sm-6 col-md-2"></div>
+                    <div className="col-12 col-sm-6 col-md-4">
                       <input
                         type="radio"
-                        class="btn-check"
+                        className="btn-check"
                         name="type"
                         value="vendor"
                         id="kt_create_account_form_type_vendor"
                         onchange="register_show_form(this.value)"
                       />
                       <label
-                        class="btn btn-active-light-warning p-4 d-flex align-items-center mb-4"
+                        className="btn btn-active-light-warning p-4 d-flex align-items-center mb-4"
                         for="kt_create_account_form_type_vendor"
                         style={{ border: "2px dashed" }}
                       >
-                        <span class="svg-icon svg-icon-3x me-3">
+                        <span className="svg-icon svg-icon-3x me-3">
                           <svg
                             width="24"
                             height="24"
@@ -185,28 +187,28 @@ function Register() {
                           </svg>
                         </span>
 
-                        <span class="d-block fw-semibold text-start">
-                          <span class="member-text text-dark d-block mb-1">
+                        <span className="d-block fw-semibold text-start">
+                          <span className="member-text text-dark d-block mb-1">
                             Vendor
                           </span>
-                          <span class="account-text">Vendor account.</span>
+                          <span className="account-text">Vendor account.</span>
                         </span>
                       </label>
                     </div>
-                    <div class="col-12 col-sm-6 col-md-2"></div>
+                    <div className="col-12 col-sm-6 col-md-2"></div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="d-flex flex-stack pt-4">
-              <div class="mr-2">
+            <div className="d-flex flex-stack pt-4">
+              <div className="mr-2">
                 <button
                   type="button"
-                  class="btn btn-lg btn-light-warning me-3"
+                  className="btn btn-lg btn-light-warning me-3"
                   onclick="window.location.href = `https://partlinks.com.au`"
                 >
-                  <span class="svg-icon svg-icon-4 me-1">
+                  <span className="svg-icon svg-icon-4 me-1">
                     <svg
                       width="24"
                       height="24"

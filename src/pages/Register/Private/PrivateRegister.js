e4a1275d5595a548b4f42427e194e6e70fd51481
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-15 00:08:50 +0330

    create register pages for different account types

---
+++ src/pages/Register/Private/PrivateRegister.js
@@ -0,0 +1,8 @@
+import React from "react";
+import RegisterForm from "../../../components/Register/RegisterForm";
+
+function PrivateRegister() {
+  return <RegisterForm accountType={"Private Member"} />;
+}
+
+export default PrivateRegister;
---
+++ src/pages/Register/Vendor/VendorRegister.js
@@ -0,0 +1,8 @@
+import React from "react";
+import RegisterForm from "../../../components/Register/RegisterForm";
+
+function VendorRegister() {
+  return <RegisterForm accountType={"Vendor"} />;
+}
+
+export default VendorRegister;

e5c53b89c5b86c6d227ad2b24bd0d54be6f28aa7
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-15 21:57:14 +0330

    create private account info completion component

---
+++ src/pages/Register/Private/PrivateComplete.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Complete from "../../../components/Register/Complete/Complete";
+
+function PrivateComplete() {
+  return <Complete memberType="private" />;
+}
+
+export default PrivateComplete;

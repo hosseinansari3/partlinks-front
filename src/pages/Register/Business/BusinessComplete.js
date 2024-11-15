29d4bbaf099a865160ea286595b652fe1b9e0784
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-15 21:56:57 +0330

    create business account info completion component

---
+++ src/pages/Register/Business/BusinessComplete.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Complete from "../../../components/Register/Complete/Complete";
+
+function BusinessComplete() {
+  return <Complete memberType="business" />;
+}
+
+export default BusinessComplete;

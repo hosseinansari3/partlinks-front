742f496b0bf5fed8ee2a82435bdc932b969d0e5f
Hossein Ansari <hosseinansari3@gmail.com>
2024-11-28 00:28:04 +0330

    fix white space at bottom, send auth Info in navigate state

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -72,7 +72,7 @@ function Login() {
       setResponseData(response.data);
       if (response?.data?.result?.match) {
         navigate("/auth/register", {
-          state: { token: response?.data?.result?.auth_info?.token },
+          state: { auth_info: response?.data?.result?.auth_info },
         });
       }
 
@@ -271,120 +271,123 @@ function Login() {
       : null;
 
   return (
-    <div className="d-flex flex-column flex-root bg-dark ">
-      <div className="d-flex flex-column flex-column-fluid flex-lg-row">
-        <div className="d-flex flex-center col-lg-6 justify-content-center align-items-center w-lg-50 pt-5 pt-lg-0 px-4">
-          <div className="d-flex flex-center flex-lg-start flex-column">
-            <a href="https://partlinks.com.au" className="mb-7">
-              <img
-                alt="Logo"
-                src="https://partlinks.com.au/panel/media/logos/custom-1.png"
-              />
-            </a>
-
-            <h2 className="text-white fw-normal fs-6 m-0">2024 ©Copyright</h2>
-          </div>
-        </div>
-        <div className="card-container d-flex flex-center col-lg-6 justify-content-center align-items-center w-lg-50 p-5">
-          <div className="card rounded-3 w-md-550px">
-            <div className="card-body p-4 p-lg-5">
-              <form
-                className="form w-100 fv-plugins-bootstrap5 fv-plugins-framework"
-                onSubmit={handleSubmit(onSubmit, onError)}
-              >
-                <input
-                  type="hidden"
-                  name="_token"
-                  value="uN7gYwMmJKf58ryRhT5OlgEbvguPj3Z7TMbbx2sz"
+    <>
+      <div className="bg-dark w-100 h-100 position-absolute"></div>
+      <div className="d-flex flex-column flex-root bg-dark  ">
+        <div className="d-flex flex-column flex-column-fluid flex-lg-row z-3">
+          <div className="d-flex flex-center col-lg-6 justify-content-center align-items-center w-lg-50 pt-5 pt-lg-0 px-4">
+            <div className="d-flex flex-center flex-lg-start flex-column">
+              <a href="https://partlinks.com.au" className="mb-7">
+                <img
+                  alt="Logo"
+                  src="https://partlinks.com.au/panel/media/logos/custom-1.png"
                 />
-                <div className="text-center mb-5">
-                  <div
-                    className={`d-flex position-relative ${
-                      step == steps.STEP_PHONE
-                        ? "justify-content-center"
-                        : "justify-content-between"
-                    } align-items-center`}
-                  >
-                    <div className="d-flex align-items-center justify-content-center">
-                      <i
-                        onClick={() => {
-                          setStep(steps.STEP_PHONE);
-                          clearErrors("otp");
-                        }}
-                        class={` ${
-                          step == steps.STEP_PHONE && "d-none"
-                        } bx bx-arrow-back back-btn`}
-                      ></i>
+              </a>
+
+              <h2 className="text-white fw-normal fs-6 m-0">2024 ©Copyright</h2>
+            </div>
+          </div>
+          <div className="card-container d-flex flex-center col-lg-6 justify-content-center align-items-center w-lg-50 p-5">
+            <div className="card rounded-3 w-md-550px">
+              <div className="card-body p-4 p-lg-5">
+                <form
+                  className="form w-100 fv-plugins-bootstrap5 fv-plugins-framework"
+                  onSubmit={handleSubmit(onSubmit, onError)}
+                >
+                  <input
+                    type="hidden"
+                    name="_token"
+                    value="uN7gYwMmJKf58ryRhT5OlgEbvguPj3Z7TMbbx2sz"
+                  />
+                  <div className="text-center mb-5">
+                    <div
+                      className={`d-flex position-relative ${
+                        step == steps.STEP_PHONE
+                          ? "justify-content-center"
+                          : "justify-content-between"
+                      } align-items-center`}
+                    >
+                      <div className="d-flex align-items-center justify-content-center">
+                        <i
+                          onClick={() => {
+                            setStep(steps.STEP_PHONE);
+                            clearErrors("otp");
+                          }}
+                          class={` ${
+                            step == steps.STEP_PHONE && "d-none"
+                          } bx bx-arrow-back back-btn`}
+                        ></i>
+                      </div>
+                      <div className="d-flex align-items-center justify-content-center">
+                        <h1 className="text-dark fw-bolder mb-3">
+                          Sing in|Sign up
+                        </h1>
+                      </div>
                     </div>
-                    <div className="d-flex align-items-center justify-content-center">
-                      <h1 className="text-dark fw-bolder mb-3">
-                        Sing in|Sign up
-                      </h1>
+
+                    <div className="text-gray-500 fw-normal fs-6">
+                      Login to your account or create one
                     </div>
                   </div>
 
-                  <div className="text-gray-500 fw-normal fs-6">
-                    Login to your account or create one
+                  {formContent}
+
+                  <div className="fv-row mb-8">
+                    <label className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        name="remember"
+                        id="remember"
+                      />
+                      <span className="form-check-label fw-normal text-gray-700 fs-base ms-1">
+                        Remember Me
+                      </span>
+                    </label>
                   </div>
-                </div>
 
-                {formContent}
+                  <div className="d-flex justify-content-between align-items-center flex-wrap gap-3  mb-4">
+                    <div></div>
+                    <Link to="/password/reset" className="link-primary">
+                      Forgot Your Password?
+                    </Link>
+                  </div>
 
-                <div className="fv-row mb-8">
-                  <label className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      name="remember"
-                      id="remember"
-                    />
-                    <span className="form-check-label fw-normal text-gray-700 fs-base ms-1">
-                      Remember Me
-                    </span>
-                  </label>
-                </div>
-
-                <div className="d-flex justify-content-between align-items-center flex-wrap gap-3  mb-4">
-                  <div></div>
-                  <Link to="/password/reset" className="link-primary">
-                    Forgot Your Password?
-                  </Link>
-                </div>
-
-                <div className="d-grid mb-4">
-                  <button
-                    type="submit"
-                    id="kt_sign_in_submit"
-                    className="btn btn-primary"
-                  >
-                    <span
-                      className={`${
-                        loading ? "d-none" : "d-block"
-                      } indicator-label`}
+                  <div className="d-grid mb-4">
+                    <button
+                      type="submit"
+                      id="kt_sign_in_submit"
+                      className="btn btn-primary"
                     >
-                      {step == steps.STEP_PHONE
-                        ? "check"
-                        : step == steps.STEP_PASSWORD
-                        ? "login"
-                        : "submit"}
-                    </span>
+                      <span
+                        className={`${
+                          loading ? "d-none" : "d-block"
+                        } indicator-label`}
+                      >
+                        {step == steps.STEP_PHONE
+                          ? "check"
+                          : step == steps.STEP_PASSWORD
+                          ? "login"
+                          : "submit"}
+                      </span>
 
-                    <span
-                      className={`${
-                        loading ? "d-block" : "d-none"
-                      } indicator-progress`}
-                    >
-                      Please wait...
-                      <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
-                    </span>
-                  </button>
-                </div>
-              </form>
+                      <span
+                        className={`${
+                          loading ? "d-block" : "d-none"
+                        } indicator-progress`}
+                      >
+                        Please wait...
+                        <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                      </span>
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
